a4890408c926e8a0be507353930a764325d2f501
Arthuremilio <arthur.ecf@hotmail.com>
2024-06-06 21:56:44 -0300

    FEAT: Remoção da tela create-user e correções na tela create-account

--- src/view/script/script.js
+++ src/view/script/script.js
@@ -19,20 +19,81 @@ document.addEventListener('DOMContentLoaded', function() {
           button.style.backgroundColor = '';
         }
       });
-    });
+    })});
   
-    hamburgerMenu.addEventListener('click', function() {
-      menu.classList.toggle('open'); 
-      overlay.style.display = menu.classList.contains('open') ? 'block' : 'none'; 
-    });
+    // hamburgerMenu.addEventListener('click', function() {
+    //   menu.classList.toggle('open'); 
+    //   overlay.style.display = menu.classList.contains('open') ? 'block' : 'none'; 
+    // });
   
-    overlay.addEventListener('click', function() {
-      menu.classList.remove('open'); 
-      overlay.style.display = 'none';
-    });
+    // overlay.addEventListener('click', function() {
+    //   menu.classList.remove('open'); 
+    //   overlay.style.display = 'none';
+    // });
   
-    closeMenuButton.addEventListener('click', function() {
-      menu.classList.remove('open'); 
-      overlay.style.display = 'none';
+  //   closeMenuButton.addEventListener('click', function() {
+  //     menu.classList.remove('open'); 
+  //     overlay.style.display = 'none';
+  //   });
+  // });
+
+  // CONSUMINDO AS APIS DO BACKEND
+
+  //cadastrar usuário 
+  document.addEventListener('DOMContentLoaded', function () {
+    const forms = document.querySelectorAll('.create-account-form');
+
+    forms.forEach(function(form) {
+        form.addEventListener('submit', function (event) {
+            console.log("Evento de envio acionado!");
+            event.preventDefault();
+
+            const senha = document.getElementById('senha').value;
+            const repetirSenha = document.getElementById('repetirSenha').value;
+            const dataAtual = new Date();
+            const vencimento = new Date(dataAtual.setMonth(dataAtual.getMonth() + 1)).toISOString().split('T')[0];
+
+            if (senha !== repetirSenha) {
+                alert('As senhas não coincidem.');
+                return;
+            }
+
+            const data = {
+                loginUsuario: document.getElementById('usuario').value,
+                nomeUsuario: document.getElementById('nome').value,
+                senha: senha,
+                email: document.getElementById('email').value,
+                cpf: document.getElementById('documento').value,
+                cep: document.getElementById('cep').value,
+                rua: document.getElementById('rua').value,
+                bairro: document.getElementById('bairro').value,
+                numeroresidencia: document.getElementById('numero').value,
+                telefone: document.getElementById('telefone').value,                
+                cidade: document.getElementById('cidade').value,
+                pais: document.getElementById('pais').value,
+                estado: document.getElementById('estado').value,
+                dataNascimento: document.getElementById('dataNascimento').value,         
+                vencimento: vencimento                                                                 
+            };
+            console.log("data criado para o usuario:" + data.loginUsuario);
+            fetch('http://localhost:8080/api/usuarios', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            })
+            .then(response => response.json())
+            .then(data => {
+                console.log('Success:', data.usuario);
+                alert('Usuário criado com sucesso!');
+                form.reset();
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+                alert('Erro ao criar usuário.');
+            });
+        });
     });
-  });
+});
+
