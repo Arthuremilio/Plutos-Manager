7aa98fafb2f800c0f7911b02d452f15a06260eb1
Arthuremilio <arthur.ecf@hotmail.com>
2024-06-09 11:38:10 -0300

    FEAT: Adição da validação de cpf no cadastro de usuário

--- src/view/script/script.js
+++ src/view/script/script.js
@@ -20,10 +20,91 @@ document.addEventListener('DOMContentLoaded', function() {
         }
       });
     })});
+
+    // Formatar a data para padrão BR
     function formatarDataParaBR(data) {
       const [ano, mes, dia] = data.split('-');
       return `${dia}/${mes}/${ano}`;
   }
+  
+    // Validação de CPF  
+    document.addEventListener('DOMContentLoaded', function() {
+      console.log("DOM totalmente carregado e analisado");
+  
+      function validarCPF(cpf) {
+          cpf = cpf.replace(/[^\d]/g, '');
+          console.log("CPF formatado:", cpf);
+  
+          if (cpf.length !== 11 || /^(\d)\1+$/.test(cpf)) {
+              console.log("CPF inválido: tamanho errado ou sequência de dígitos repetidos");
+              return false;
+          }
+  
+          let soma = 0;
+          for (let i = 0; i < 9; i++) {
+              soma += parseInt(cpf.charAt(i)) * (10 - i);
+          }
+  
+          let resto = 11 - (soma % 11);
+          let digitoVerificador = resto === 10 || resto === 11 ? 0 : resto;
+  
+          if (digitoVerificador !== parseInt(cpf.charAt(9))) {
+              console.log("Primeiro dígito verificador inválido");
+              return false;
+          }
+  
+          soma = 0;
+          for (let i = 0; i < 10; i++) {
+              soma += parseInt(cpf.charAt(i)) * (11 - i);
+          }
+  
+          resto = 11 - (soma % 11);
+          digitoVerificador = resto === 10 || resto === 11 ? 0 : resto;
+  
+          if (digitoVerificador !== parseInt(cpf.charAt(10))) {
+              console.log("Segundo dígito verificador inválido");
+              return false;
+          }
+
+          return true;
+      }
+  
+      function validarCampoCPF() {
+          let campoCPF = document.getElementById('documento');
+          let hint = document.getElementById('cpf-hint');
+          if (!campoCPF) {
+              console.log("Elemento campoCPF não encontrado");
+              return;
+          }
+  
+          console.log("Elemento campoCPF encontrado:", campoCPF);
+  
+          campoCPF.addEventListener("change", function() {
+              let cpf = campoCPF.value;
+              console.log("Validando CPF:", cpf);
+  
+              if (validarCPF(cpf)) {
+                campoCPF.setCustomValidity('CPF válido');
+                campoCPF.style.borderColor = 'green';
+                campoCPF.style.backgroundColor = 'green';
+                hint.innerHTML  = 'CPF válido <i class="fas fa-check-circle fa-lg"></i>';
+                hint.style.color = 'green';
+            } else {
+                campoCPF.setCustomValidity('CPF inválido');
+                campoCPF.style.borderColor = 'red';
+                campoCPF.style.backgroundColor = 'red';
+                hint.innerHTML  = 'CPF inválido <i class="fas fa-times-circle fa-lg"></i>';
+                hint.style.color = 'red';
+            }
+          });
+      }
+  
+      validarCampoCPF();
+        
+  });
+  
+
+  
   
     // hamburgerMenu.addEventListener('click', function() {
     //   menu.classList.toggle('open'); 
@@ -49,16 +130,21 @@ document.addEventListener('DOMContentLoaded', function() {
 
     forms.forEach(function(form) {
         form.addEventListener('submit', function (event) {
-            console.log("Evento de envio acionado!");
             event.preventDefault();
 
             const senha = document.getElementById('senha').value;
             const repetirSenha = document.getElementById('repetirSenha').value;
             const dataAtual = new Date();
             const vencimento = new Date(dataAtual.setMonth(dataAtual.getMonth() + 1)).toISOString().split('T')[0];
+            const hint = document.getElementById('cpf-hint');
 
             if (senha !== repetirSenha) {
-                alert('As senhas não coincidem.');
+                alert('As senhas não coincidem!');
+                return;
+            }
+
+            if (hint.style.color === 'red') {
+                alert('CPF informado é inválido!');
                 return;
             }
 
